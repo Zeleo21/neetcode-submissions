69dddedb9eb9551201389cbce81a4f113fa38ecd
Léo Guazzaroni <leoguazza@gmail.com>
2026-08-16 12:15:54 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length) return false;
+        const sCharCounts = new Map<string, number>;
+        const tCharCounts = new Map<string, number>;
+        for(const char of s) {
+            sCharCounts.has(char) ? sCharCounts.set(char, sCharCounts.get(char) + 1) : sCharCounts.set(char, 1);
+        }
+        for(const char of t) {
+            tCharCounts.has(char) ? tCharCounts.set(char, tCharCounts.get(char) + 1) : tCharCounts.set(char, 1);
+        }
+        for(const [key, val] of sCharCounts) {
+            if(!tCharCounts.has(key)) return false;
+            if(tCharCounts.get(key) !== val) return false;
+        }
+        return true;
+    }
+}
